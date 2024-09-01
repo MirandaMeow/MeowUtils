435ffc59d2cdc0ce0e804aad92d6e3a605dc3a14
MirandaMeow <444037570@qq.com>
2024-09-01 09:22:43 +0800

    optimize Murder Module

--- src/fun/miranda/MeowUtils.java
+++ src/fun/miranda/MeowUtils.java
@@ -6,7 +6,6 @@ import fun.miranda.ImprintScroll.CommandImprint;
 import fun.miranda.ImprintScroll.CommandImprintSign;
 import fun.miranda.ImprintScroll.EventImprint;
 import fun.miranda.Murder.CommandMurder;
-import fun.miranda.Murder.CommandMurderBlock;
 import fun.miranda.Murder.EventShowEntityType;
 import fun.miranda.RollDice.EventRollDice;
 import fun.miranda.SealScroll.CommandSeal;
@@ -165,7 +164,6 @@ public class MeowUtils extends JavaPlugin {
      */
     private void setupMurder() {
         registerCommand("murder", new CommandMurder());
-        registerCommand("murderblock", new CommandMurderBlock());
         registerEvent(new EventShowEntityType());
         logger.info("§b[§6猫子组件§b] §e  -- 实体清理");
     }
--- src/fun/miranda/Murder/CommandMurder.java
+++ src/fun/miranda/Murder/CommandMurder.java
@@ -1,6 +1,7 @@
 package fun.miranda.Murder;
 
 import fun.miranda.Utils.Utils;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -10,39 +11,45 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class CommandMurder implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("该命令只能由玩家执行");
+        Murder murder;
+        if (!(sender instanceof Player) && !(sender instanceof BlockCommandSender)) {
+            sender.sendMessage("该命令只能由玩家或命令方块执行");
             return true;
         }
         if (args.length < 2) {
-            player.sendMessage("§e用法: §6/murder §b<distance> <EntityType...>");
+            sender.sendMessage("§e用法: §6/murder §b<distance> <EntityType...>");
             return true;
         }
         String distanceString = args[0];
         if (!Utils.isInt(distanceString)) {
-            player.sendMessage("§c输入的距离不正确");
+            sender.sendMessage("§c输入的距离不正确");
             return true;
         }
         int distance = Integer.parseInt(distanceString);
         if (distance <= 0) {
-            player.sendMessage("§c输入的距离不正确");
+            sender.sendMessage("§c输入的距离不正确");
             return true;
         }
-        Murder murder = new Murder(player, distance);
+        if (sender instanceof Player) {
+            murder = new Murder((Player) sender, distance);
+        } else {
+            murder = new Murder((BlockCommandSender) sender, distance);
+        }
         int success = 0;
         for (int i = 1; i < args.length; i++) {
             success += murder.modifyType(args[i]);
         }
         if (success != 0) {
-            player.sendMessage("§c输入的实体不正确");
+            sender.sendMessage("§c输入的实体不正确");
             return true;
         }
         int count = murder.kill();
-        player.sendMessage(String.format("§e清除了 §b%d §e个实体", count));
+        sender.sendMessage(String.format("§e清除了 §b%d §e个实体", count));
         return true;
     }
 
@@ -53,11 +60,21 @@ public class CommandMurder implements TabExecutor {
         for (EntityType type : entityTypes) {
             entityTypesString.add(type.name());
         }
+        entityTypesString.add("ALL");
         if (args.length == 1) {
             return new ArrayList<>();
         }
         if (args.length > 1) {
-            return Utils.listFilter(entityTypesString, args[args.length - 1].toUpperCase(Locale.ROOT));
+            String arg = args[args.length - 1].toUpperCase(Locale.ROOT);
+            if (arg.startsWith("-")) {
+                for (int i = 0; i < entityTypesString.size(); i++) {
+                    if (Objects.equals(entityTypesString.get(i), "ALL")) {
+                        continue;
+                    }
+                    entityTypesString.set(i, "-" + entityTypesString.get(i));
+                }
+            }
+            return Utils.listFilter(entityTypesString, arg);
         }
         return new ArrayList<>();
     }
